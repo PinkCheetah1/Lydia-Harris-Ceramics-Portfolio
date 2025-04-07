7f0c4719c53f437d0bbd982f0ba0c3a55c970253
PinkCheetah1 <66535303+PinkCheetah1@users.noreply.github.com>
2025-04-07 00:14:04 -0600

    work on social media links

--- lydia-harris-ceramics-2/src/components/SocialMediaLink.jsx
+++ lydia-harris-ceramics-2/src/components/SocialMediaLink.jsx
@@ -6,12 +6,12 @@ function SocialMediaLink({
     text="[No Social Media Name Available]", 
     socialLink=""}) {
     return(
-        <>
+        <div className="social-link">
             <a href={socialLink}>   
-                <img className="icon" src={iconLink} alt={altText}></img>
-                <h3>{text}</h3>
+                <img className="social-link-icon" src={iconLink} alt={altText}></img>
+                <h3 className="social-link-text">{text}</h3>
             </a>
-        </>
+        </div>
         )
     }
 
