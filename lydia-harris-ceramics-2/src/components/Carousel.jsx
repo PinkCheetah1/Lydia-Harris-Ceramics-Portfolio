d1333ed7794f397743c71c6c84a69c4a009cc1e0
PinkCheetah1 <66535303+PinkCheetah1@users.noreply.github.com>
2025-04-07 22:04:23 -0600

    Carousel image

--- lydia-harris-ceramics-2/src/components/Carousel.jsx
+++ lydia-harris-ceramics-2/src/components/Carousel.jsx
@@ -4,8 +4,13 @@ function Carousel() {
     return(
         // TODO: Make working Carousel
         // TODO: Add text/button on top of Carousel
-        <>
-        </>
+        <section>
+            <img className="carousel-image" src="https://www.rockgardenpottery.com/wp-content/uploads/2019/04/Pottery-And-Ceramics-1024x683.jpg" 
+            alt="Just some random pottery"></img>
+            <div>
+                
+            </div>
+        </section>
     )
 }
 
