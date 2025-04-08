dd92237c9a19b149abdcc4a2275da91ae19493d9
PinkCheetah1 <66535303+PinkCheetah1@users.noreply.github.com>
2025-04-07 22:34:01 -0600

    Nav button on Carousel!

--- lydia-harris-ceramics-2/src/components/Carousel.jsx
+++ lydia-harris-ceramics-2/src/components/Carousel.jsx
@@ -1,14 +1,16 @@
 import "./Carousel.css";
+import NavButton from "./NavButton";
 
 function Carousel() {
     return(
         // TODO: Make working Carousel
         // TODO: Add text/button on top of Carousel
         <section>
-            <img className="carousel-image" src="https://www.rockgardenpottery.com/wp-content/uploads/2019/04/Pottery-And-Ceramics-1024x683.jpg" 
-            alt="Just some random pottery"></img>
-            <div>
-                
+            <div className="carousel">
+                <img className="carousel-image" src="https://www.rockgardenpottery.com/wp-content/uploads/2019/04/Pottery-And-Ceramics-1024x683.jpg" 
+                alt="Just some random pottery">                
+            </img>
+            <NavButton to="/" text="View Gallery" className="carousel-button"></NavButton>
             </div>
         </section>
     )
--- lydia-harris-ceramics-2/src/components/NavButton.jsx
+++ lydia-harris-ceramics-2/src/components/NavButton.jsx
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom";
 import "./NavButton.css";
 
-function NavButton({ to = "/", text = "notext"}) {
+function NavButton({ to = "/", text = "notext", className="nav-button"}) {
     return (
-        <Link to={to} className="nav-button"> 
-        {text}
-        </Link>
+        <Link to={to} className={`nav-button ${className}`}>
+      {text}
+    </Link>
     )
 }
 
